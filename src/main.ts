57868c214cf334fb9ae374d625fda9532baf9ac0
Alisher <alishersharipov670@gmail.com>
2026-08-15 14:11:49 +0500

    Updated

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './module/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3030);
+  await app.listen(process.env.PORT ?? 3033);
 }
 bootstrap();
